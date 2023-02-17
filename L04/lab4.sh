69953df0f58fdcedf97cc2e9f4d70c253c3e47d8
nakamurs <nakamurs@mcmaster.ca>
2023-02-17 16:58:30 -0500

    lab4Done

--- L04/lab4.sh
+++ L04/lab4.sh
@@ -3,34 +3,35 @@
 # Use First Argument As Working Directory
 WDIR="$1"
 cd "$WDIR"
-FILE="file1.txt"
+FILENAME="file1.txt"
+
+if [ "$#" -ge 3 ] ; then
+    FILENAME=$3
+fi
 
 if [ "$2" -eq 1 ] ; then
-    if [-f $FILE ] ; then
-	:
-    else
-	touch $FILE
-	echo "$USER" > file1.txt
+    if [ ! -f $FILENAME ] ; then
+	echo $USER > $FILENAME
     fi
 elif [ "$2" -eq 2 ] ; then
-    if [-e $FILE] ; then
-	mkdir backup
-	cp $FILE /backup
+    if [ -f $FILENAME ] ; then
+	mkdir ./backup
+	cp $FILENAME ./backup
     fi
 fi
 
 if [ "$2" -eq 3 ] ; then
-    if [-e "backup/"$FILE] ; then
-	cp "backup/"$FILE /L04
-	rm backup/$FILE
+    if [ -f ./backup/$FILENAME ] ; then
+	cp ./backup/$FILENAME $FILENAME
+	rm ./backup/$FILENAME
     fi
 fi
 
-if ["$#" -ge 3] ; then
-    FILE="$3"
+if [ "$#" -gt 3 ] ; then
+    echo $4 > excess.txt
+    for ((i=5 ; i <= "$#" ; i++ )) ; do
+	echo ${!i} >> excess.txt
+    done
 fi
-
-
-    
-
+   
 echo "Finished"
